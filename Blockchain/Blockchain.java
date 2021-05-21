410bf8d8cf5822852cc5eecadbb2833070d3b3ac
Akshay Patel <apatel1995@hotmail.com>
2021-05-20 20:29:53 -0700

    added random string utility

--- Blockchain/Blockchain.java
+++ Blockchain/Blockchain.java
@@ -2,12 +2,39 @@ import com.google.gson.*;
 import java.io.*;
 import java.net.*;
 import java.util.*;
+import java.security.*;
 
 
 class Blockchain{
 
     public static void main(String[] args) {
         
+    }
+
+
+}
+
+class utils{
+
+    //borrowed from the class code
+    //credit to:
+    //https://www.quickprogrammingtips.com/java/how-to-generate-sha256-hash-in-java.html  @author JJ
+    //https://dzone.com/articles/generate-random-alpha-numeric  by Kunal Bhatia  ·  Aug. 09, 12 · Java Zone
+    private static final String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    public static String randomAlphaNumeric(int count) {
+		StringBuilder builder = new StringBuilder();
+		while (count-- != 0) {
+			int character = (int)(Math.random()*ALPHA_NUMERIC_STRING.length());
+			builder.append(ALPHA_NUMERIC_STRING.charAt(character));
+		}
+		return builder.toString();
+    }
+    public static String ByteArrayToString(byte[] ba){
+		StringBuilder hex = new StringBuilder(ba.length * 2);
+		for(int i=0; i < ba.length; i++){
+			hex.append(String.format("%02X", ba[i]));
+		}
+		return hex.toString();
     }
 
 }
